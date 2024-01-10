ce3b21a27f9c7c7179955f7c77de1aceb8a92274
Elroy Toscano <43146754+elroytoscano@users.noreply.github.com>
2024-01-10 15:53:12 +0530

    added styled-components as css framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { styled } from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #bf4f74;
+`;
+
 function App() {
   return (
     <div>
-      <header>
+      <Title>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
-      </header>
+      </Title>
     </div>
   );
 }
